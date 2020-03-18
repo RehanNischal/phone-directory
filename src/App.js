eb4a015bc71ee12bf8f66491882c91b41d513f81
Rehan Nischal <rehannischal@gmail.com>
2020-03-19 01:41:37 +0530

    Added ‘Delete’ button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 import Header from "./Header";
-
+import "./App.css";
 class App extends Component {
-  render(){
-    let x=1;
-    let y=9;
-  return (
-    <div >
-      <Header/>
-      <button>Add</button>  
-      <div>
-        <span>Name</span><br/>
-        <span>Phone</span>
-      </div>
-    
-                              {/* <label htmlFor="Name">Name: </label>
-                                <input type="text" id="Name" placeholder="Type your name here" defaultValue={x+y}></input> */}
+ 
+  render() {
+    let subscribers=[
+      {
+        id:1,name:"rehan",phone:"9996305732"
+      },
+      {
+       id:2, name:"harshit",phone:"8572829003"
+      }
+    ]
+        return(
+          <div className="component-container">
+           <Header/>
 
-                            
-</div>
-);
-      
+
+          <div className="component-body-container">
+            <button className="custom-btn add-btn">Add</button>
+
+            <div className="grid-container heading-container">
+              <span className="grid-item name-heading">Name</span>
+              <span className="grid-item phone-heading">Phone</span>
+            </div>
+        
+{
+  subscribers.map(sub=>{
+    return <div key={sub.id} className="grid-container">
+      <span className="grid-item">{sub.name}</span>
+      <span className="grid-item">{sub.phone}</span>
+      <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+      </div>
+  })
+}
+
+
+          </div>
+          
+        
+          </div>
+        
+
+        
+        );
   
 
-
-       }
+}
 }
 
 export default App;
