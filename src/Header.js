93f156d044141ea460a1f4076c2eace12a076848
Rehan Nischal <rehannischal@gmail.com>
2020-03-18 15:05:20 +0530

    Styled ‘Header’ (External Styling)
    Signed-off-by: Rehan Nischal <rehannischal@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React, { Component }from 'react';
+import './Header.css'
 //functional component
 const Header = function(){
-    const HeaderStyle= {textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase'};
+   
     return(
-        <div style={HeaderStyle}>
+        <div className="header" >
 
       Phone Directory
       </div>
